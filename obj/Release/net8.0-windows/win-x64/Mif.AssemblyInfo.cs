e4b06e3b1aa444bf87566cfb356f70a90791a06e
Nickstar <nickstar32123@gmail.com>
2026-03-02 13:30:43 -0400

    tons of stuff

--- obj/Release/net8.0-windows/win-x64/Mif.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/win-x64/Mif.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mif")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81bdcc2ba1755d3aaee9f43d44d2292d3aeb6bc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mif")]
